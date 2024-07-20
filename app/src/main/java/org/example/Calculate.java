6a8c422158707665dd0a2c0084f32b88c01de93c
Asuma <g.asuma0105@gmail.com>
2024-07-21 02:38:57 +0900

    課題の実装

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,42 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public int sumAll(int x, int y) {
+    int sum = 0;
+    for (int i = x; i <= y; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+
+  public int sumPart(int x, int y) {
+    int sum = 0;
+    for (int i = x; i <= y; i = i + 2) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+
+  public void Calcu() {
+    int x = 2, y = 3;
+    for (int i = 0; i < 10; i++) {
+      if (y > x && x > 0) {
+        if (y == x + 1) {
+          System.out
+              .println("Sum of " + x + " and " + y + " is " + sum(x, y) + " .Average is " + (sum(x, y) / 2) + " .");
+        } else {
+          System.out.println(
+              "Sum of " + x + " to " + y + " is " + sumAll(x, y) + " .Average is " + sumAll(x, y) / (y - x + 1) + " .");
+          if (x % 2 == 1) {
+            System.out.println("Sum of odd of " + x + " to " + y + " is " + sumPart(x, y) + " .Sum of even is "
+                + sumPart(x + 1, y) + " .");
+          }
+        }
+      }
+      x = 1;
+      y++;
+    }
+
+  }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,20 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testSumAll() {
+    Calculate calculate = new Calculate();
+    int expected = 55;
+    assertEquals(expected, calculate.sumAll(1, 10));
+  }
+
+  @Test
+  public void testSumPart() {
+    Calculate calculate = new Calculate();
+    int expected = 9;
+    assertEquals(expected, calculate.sumPart(1, 5));
+    expected = 6;
+    assertEquals(expected, calculate.sumPart(2, 5));
+  }
 }
